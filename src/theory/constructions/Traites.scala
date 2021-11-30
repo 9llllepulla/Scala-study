fd91bc27caa4b223a51bb483a2f3f75b43b0773a
sergeylyashko <slyashko@mail.ru>
2021-11-30 21:00:44 +0300

    add traits

--- src/theory/constructions/Traites.scala
+++ src/theory/constructions/Traites.scala
@@ -1,22 +1,27 @@
 package theory.constructions
 
-object Traites {
+trait Philosophical {
+
+  def philosophidze() = {
+    println("На меня тратится память, следовательно, я существую!")
+  }
+}
+
+class Frog extends Animal with Philosophical {
+  override def toString: String = "зеленая"
+
+  override def philosophidze(): Unit = {
+    println("Мне живется не легко, потому что я " + toString + "!")
+  }
+}
+
+class Animal
+trait HasLeg
+
+object Traites extends App {
+
+  val frog = new Frog()
+  frog.philosophidze() // Мне живется не легко, потому что я зеленая!
 
 }
 
-abstract class AbstractImpl(void: Unit) {
-
-}
-
-trait LikeJavaInterface {
-  def realization: Int
-}
-
-trait ManyRealization {
-  def realization: Int
-}
-
-class ImplementationTraits(void: Unit) extends LikeJavaInterface with ManyRealization {
-
-  override def realization: Int = ???
-}
