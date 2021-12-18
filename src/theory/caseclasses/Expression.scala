c01da454f86a6bb1d7589d96eb2227d91e926e90
sergeylyashko <slyashko@mail.ru>
2021-12-18 19:26:58 +0300

    add case classes example

---
+++ src/theory/caseclasses/Expression.scala
@@ -0,0 +1,48 @@
+package theory.caseclasses
+
+abstract class Expression
+
+/**
+ * В case classes добавляются по-умолчанию:
+ * <p>
+ *    -- фабричный метод;
+ * </p><p>
+ *    -- все аргументы получают префикс val и к ним можно обращаться как полям;
+ * </p><p>
+ *    -- класс получает реализацию toString. hashCode, equals.
+ * </p><p>
+ *    -- компилятор добавляет метод <i><b>copy</i></b> к классу для его изменения
+ */
+case class Var(name: String) extends Expression
+
+case class Number(number: Double) extends Expression
+
+case class UnaryOperation(operator: String, arg: Expression) extends Expression
+
+case class BinaryOperation(operator: String, left: Expression, right: Expression) extends Expression
+
+object Expr extends App {
+
+  val newVar = Var("x") // new Var("x")
+
+  val op = BinaryOperation("+", Number(1), newVar) // фабричные методы создания объектов
+  println(op) // BinaryOperation(+,Number(1.0),Var(x))
+
+  println(newVar.name) // x
+  println(op.left) // Number(1.0)
+
+  val opCopy = op.copy(operator = "-")
+  println(opCopy) // BinaryOperation(-,Number(1.0),Var(x))
+
+  /* Case classes поддерживают сопоставления с образцом */
+
+  def simplifyTop(expression: Expression): Expression = expression match {
+    case UnaryOperation("-", UnaryOperation("-", e)) => e // двойное отрицание
+    case BinaryOperation("+", e, Number(0)) => e // прибавление 0
+    case BinaryOperation("*", e, Number(1)) => e // умножение на 1
+    case _ => expression
+  }
+
+}
+
+
